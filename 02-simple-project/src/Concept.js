cdc8d8e63a538ff0776fd863bcfb4f3d4995d6c0
tranviet0710 <102190298@sv1.dut.udn.vn>
2023-09-09 18:09:36 +0700

    props component simple project

---
+++ 02-simple-project/src/Concept.js
@@ -0,0 +1,12 @@
+const Concept = (props) => {
+    return (
+
+        <li className="concept">
+            <img src={props.image} alt={props.title} />
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+        </li>
+
+    );
+}
+export default Concept;
