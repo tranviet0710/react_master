7eaf5faf0bb6bc22fea302a61aed00a3dc310b90
tranviet0710 <102190298@sv1.dut.udn.vn>
2023-10-03 22:57:43 +0700

    demo-rested-routes-error-handling

---
+++ react-router-master/demo-rested-routes-error-handling/src/pages/Root.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
+export default function RootLayout() {
+  return (
+    <>
+      <MainNavigation></MainNavigation>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
