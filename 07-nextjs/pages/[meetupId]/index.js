f3d2edb8cc90abb5e86b8a5d61767570a301e3ec
tranviet0710 <102190298@sv1.dut.udn.vn>
2023-10-06 00:31:14 +0700

    07-nextjs

--- 07-nextjs/pages/[meetupId]/index.js
+++ 07-nextjs/pages/[meetupId]/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { useRouter } from "next/router";
+import MeetupDetail from "@/components/meetups/MeetupDetail";
 export default function index() {
   const router = useRouter();
+  const meetupId = router.query.meetupId;
+  console.log(meetupId);
   return (
-    <div>
-      <h1>{router.query.meetupId}</h1>
-    </div>
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
   );
 }
--- 07-nextjs/pages/index.js
+++ 07-nextjs/pages/index.js
@@ -1,24 +1,40 @@
-import MeetupList from '../components/meetups/MeetupList';
+import { useEffect, useState } from "react";
+import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
   {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
   {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!'
-  }
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
+  },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />
+function HomePage(props) {
+  const [meetUps, setMeetUps] = useState([]);
+  useEffect(() => {
+    // send a http request and fetch data
+    setMeetUps(DUMMY_MEETUPS);
+  }, []);
+  return <MeetupList meetups={props.meetUps} />;
 }
 
+export async function getStaticProps() {
+  return {
+    props: {
+      meetUps: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
+}
 export default HomePage;
