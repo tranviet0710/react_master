516a5212c5dc1a5965ce62c3c922a8dea012eeff
tranviet0710 <102190298@sv1.dut.udn.vn>
2023-10-10 22:50:48 +0700

    post api to mongodb

--- 07-nextjs/pages/api/new-meetup.js
+++ 07-nextjs/pages/api/new-meetup.js
@@ -5,20 +5,20 @@ import { MongoClient } from "mongodb";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
+    const client = await MongoClient.connect(
+      "mongodb+srv://vietdev:WOXGUE2PMxxDrQDA@cluster0.59ezxwz.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    // have to synchro this function before close the connection
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
-  const client = await MongoClient.connect(
-    "mongodb+srv://vietdev:<Congviet2001@@>@cluster0.59ezxwz.mongodb.net/?retryWrites=true&w=majority"
-  );
-
-  const db = client.db();
-
-  const meetupsCollection = db.getCollection("meetups");
-
-  const result = meetupsCollection.insertOne(data);
-
-  console.log(result);
-
-  client.close();
-
-  res.status(201).json({ message: "Meetup inserted!" });
 }
--- 07-nextjs/pages/new-meetup/index.js
+++ 07-nextjs/pages/new-meetup/index.js
@@ -1,9 +1,23 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function index() {
-  const addMeetupHandler = (data) => {
+  const router = useRouter();
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
     console.log(data);
+
+    router.push("/");
   };
   return <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>;
 }
